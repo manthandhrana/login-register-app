90fecd67e176dd31d05f361946d8932c4674ffb4
Manthan Dhrana <dhranamanthan201318@gmail.com>
2025-06-20 17:18:04 +0530

    vercel file change

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CORS_URL, credentials: true }));
 
 // Routes
 app.get("/", (req,res)=>{
